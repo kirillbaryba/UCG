4f27052b7b1c746a3599e9babce7ee07fcf05992
kirillos <kirill.baryba@gmail.com>
2017-07-23 11:06:09 +0300

    clear package.json

--- src/js/libs.js
+++ src/js/libs.js
@@ -2,9 +2,4 @@
  Third party
  */
 //= ../../bower_components/jquery/dist/jquery.min.js
-//= ../../bower_components/bootstrap/dist/js/bootstrap.js
-//= ../../bower_components/gray/js/jquery.gray.js
-//= ../../bower_components/select2/dist/js/select2.js
-//= ../../bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js
-//= ../../bower_components/jquery-sticky/jquery.sticky.js
-//= ../../bower_components/jquery-mask-plugin/dist/jquery.mask.js
+/*//= ../../bower_components/bootstrap/dist/js/bootstrap.js*/
