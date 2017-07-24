466d902b5d7cc99b4bbb5c3c093625f40d2c145b
kirillos <kirill.baryba@gmail.com>
2017-07-24 11:07:36 +0300

    update mobile device

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,8 @@
     //toogle-collapse menu
     $('.navbar-toggle').on('click', function () {
         $(this).toggleClass('on');
-        $(this).closest('.header').find('.navbar-nav').toggleClass('is-visible');
+        $(this).closest('.header').toggleClass('header--is-visible');
+        $(this).closest('.header').find('.navbar-nav').toggleClass('navbar-nav--is-visible');
     });
 
 })();
